cbb014ba52b6abcb78535e57276ff47314c364e7
Drinqaka <qakadrin@gmail.com>
2025-09-16 20:42:04 +0200

    Moduli 26

---
+++ moduli 26/main.js
@@ -0,0 +1,88 @@
+console.log("Bye");
+
+var x = 10;
+var_valu3 = "Digitalschool";
+var a = 50+50;
+console.log(a);
+
+var b=60;
+var c=80;
+var d=b+c;
+console.log(d);
+
+var e=40;
+var f=10;
+var g=e+f;
+console.log(g);
+
+var h=100;
+var i=5;
+var j=h/1;
+console.log(j);
+
+var k=10;
+var I=4;
+var m=k%I;
+console.log(m);
+
+var n=99;;
+n++;
+console.log(n);
+
+var o=101;
+o--;
+console.log(o);
+
+var p=20;
+var q=p**2;
+console.log(q);
+
+var x=15;
+x+=5;
+console.log(x);
+
+var r=35;
+r==5;
+console.log(r);
+
+var s=10;
+s*=7;
+console.log(s);
+
+var t=200;
+t/=2;
+console.log(t);
+
+var u=10;
+u%=5;
+console.log(u);
+
+var v=5;
+var w=5;
+console.log(v==w);
+
+var x=6;
+var y=7;
+console.log(x!=y);
+
+var num1=100;
+var num2=10;
+console.log(num1>num2);
+
+var num3=1;
+var num4=2;
+console.log(num3<num4);
+
+var num5=123;
+var num6="123";
+console.log(num5 --- num6);
+
+var firstName = "Drin";
+var lastName = "Qaka";
+var fullName= "Drin Qaka";
+console.log(fullName);
+
+var school="digitalschool"
+console.log(`Hello from ${school}`);
+
+

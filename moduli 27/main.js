74124eabea2cb2bcba56b8fe3629ce61abbf53cb
Drinqaka <qakadrin@gmail.com>
2025-09-25 20:44:24 +0200

    moduli 27

---
+++ moduli 27/main.js
@@ -0,0 +1,27 @@
+function lum  (){
+    alert ("this is a warning")
+}
+
+lum()
+
+function buna(number1,number2) {
+    return number1+number2;
+}
+
+console.log(buna(10,10));
+
+function toCelsius (number1){;
+return number1*5/9;
+}
+
+console.log("54 fahrneit = "+toCelsius(54)+ "celsius")
+
+function drin() {
+var localvar="Digitalschool";
+alert(localvar);
+}
+
+drin();
+
+
+

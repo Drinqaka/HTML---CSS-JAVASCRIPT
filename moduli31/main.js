460e46f016749a30bf497f576a7a502ec829fc5c
drinqaka <qakadrin@gmail.com>
2025-10-21 20:44:14 +0200

    moduli31

---
+++ moduli31/main.js
@@ -0,0 +1,64 @@
+//do while //
+
+var i=0;
+
+do{
+    i=i+1;
+    console.log(i)
+}while(i<5);
+
+console.log('hhhhhhhhhhhhhh');
+var j=0;
+
+while(j<5){
+    j=j + 1;
+    console.log(j);
+}
+
+console.log('hhhhhhhhhhhhhh');
+
+let n=0;
+let x=0;
+
+while(n<3){
+    n++;
+    x=x+n;
+    console.log(n)
+    console.log(x)   
+}
+
+for (i=0; i<5; i++){
+    console.log('vlera e numrit eshte ' + i);
+}
+
+var person = {firstname:"lamine",lastname:"yamal",age:18};
+var text = " ";
+
+var z;
+
+for(z in person){
+    text+= person[z];
+}
+
+console.log(text);
+
+var names=['Steve','Billy the badass','african'];
+var y;
+
+for (y in names){
+    console.log(y);
+}
+
+var text= 'javascript'
+
+var l;
+
+for(l in text){
+    console.log(l)
+}
+
+
+
+
+
+

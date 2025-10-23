1f7439dcf42ac461071835c0b154bad668f255d8
Drinqaka <qakadrin@gmail.com>
2025-10-23 20:45:35 +0200

    moduli32

---
+++ moduli32/main.js
@@ -0,0 +1,38 @@
+// var text="The most famous school is                                          
+// var result=text.search("Digital School");
+
+// document.getElementById('result1').innerHTML = result;
+
+// var text="The most famous school is Digital School";                                       
+// var result=text.replace(/Digital School/,"Ismajl Qemajli");
+
+// document.getElementById('result2').innerHTML = result;
+
+// var text ="joralumidrinisufjanireisi"
+// var regex = new RegExp("joralu");
+// document.getElementById('result3').innerHTML = regex.test(text);
+
+// var text="My school is the best school of all school";
+// var regex = /school/g;
+
+// document.getElementById('result4').innerHTML = text.match(regex);
+
+
+
+// var text="My school is the best school of all school";
+// var regex = /school/g;
+
+// document.getElementById('result4').innerHTML = text.match(regex);
+
+
+//  var text="My school is the best school of all school";
+//  var regex = /[sol]/g;
+
+//  document.getElementById('result4').innerHTML = text.match(regex)
+
+ var text="the numbers 10";
+ var regex = /[0-10]/g;
+
+ document.getElementById("result6").innerHTML = text.match(regex)
+
+
